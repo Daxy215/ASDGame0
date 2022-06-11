51f2f6085371f53a0d63899492bbefa7cd704f12
Daxy215 <80177742+Daxy215@users.noreply.github.com>
2022-06-11 21:41:35 +0100

    Add files via upload

--- TDGame/TownCentre.cpp
+++ TDGame/TownCentre.cpp
@@ -1,11 +1,9 @@
-#include "TownCentre.h"
 #include<iostream>
+#include "TownCentre.h"
+#include "LoaderManager.h"
 
-TownCentre::TownCentre() : Building("TownCentre",sf::Vector2f(0,0))
-{
+TownCentre::TownCentre() : Building(getLoaderManager().getBuilding("TowerCentre"), sf::Vector2f(0, 0)) {
 	size = 150;
 	Health = 100000;
 	Components.push_back(new Component("TownCentre"));
 }
-
-

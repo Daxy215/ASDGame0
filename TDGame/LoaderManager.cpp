9bc710be820103d960682852f5bf4e212b89a960
Daxy215 <80177742+Daxy215@users.noreply.github.com>
2022-06-15 16:06:30 +0100

    Add files via upload

--- TDGame/LoaderManager.cpp
+++ TDGame/LoaderManager.cpp
@@ -1,6 +1,7 @@
 #include <json/json.h>
 #include <fstream>
 #include "LoaderManager.h"
+#include "GameData.h"
 
 void LoaderManager::loadBuildings() {
 	//Loading the data file.
@@ -9,7 +10,7 @@ void LoaderManager::loadBuildings() {
 
 	//Reading it as json.
 	cfgfile >> cfg_root;
-	
+
 	//const int buildingsSize = cfg_root.size();
 
 	//std::cout << "[DEBUG] JSON: " << cfg_root << std::endl;
@@ -18,7 +19,7 @@ void LoaderManager::loadBuildings() {
 	//Iterate throughout the buildings.
 	for (Json::Value::const_iterator itr = cfg_root.begin(); itr != cfg_root.end(); itr++) {
 		std::string buildingName = itr.name();
-		
+
 		//Setting values
 		std::string componentName = cfg_root[buildingName]["ComponentName"].asString();
 		std::string buildingType = cfg_root[buildingName]["BuildingType"].asString();
@@ -77,7 +78,7 @@ void LoaderManager::loadSoliders() {
 
 	for (Json::Value::const_iterator itr = cfg_root.begin(); itr != cfg_root.end(); itr++) {
 		std::string soldierName = itr.name();
-		
+
 		int health = cfg_root[soldierName]["Health"].asInt();
 		int attack = cfg_root[soldierName]["Attack"].asInt();
 		int size = cfg_root[soldierName]["Size"].asInt();
@@ -85,6 +86,49 @@ void LoaderManager::loadSoliders() {
 		int speed = cfg_root[soldierName]["Speed"].asInt();
 
 		soldiers.push_back(new Soldier(soldierName, health, attack, size, attackDelay, speed));
+	}
+}
+
+void LoaderManager::loadWaves() {
+	//Loading the data file.
+	Json::Value cfg_root = "";
+	std::ifstream cfgfile("Data/Waves/defualt.json");
+	//Reading it as json.
+	cfgfile >> cfg_root;
+
+	for (Json::Value::const_iterator itr = cfg_root.begin(); itr != cfg_root.end(); itr++) {
+		std::string waveName = itr.name();
+
+		Wave* wave = new Wave(waveName);
+
+		//Loop through all of the enemies in the waves
+		for (Json::Value::const_iterator itr = cfg_root[waveName]["Soldiers"].begin(); itr != cfg_root[waveName]["Soldiers"].end(); itr++) {
+			std::string soldierName = itr.name();
+
+			Soldier* soldier = getSoldier(soldierName);
+
+			if (soldier == nullptr) {
+				std::cout << "[DEBUG] Couldn't find an enemy with the name of " << soldier << " inside wave with a name of " << waveName << std::endl;
+
+				continue;
+			}
+
+			int amount = cfg_root[waveName]["Soldiers"][soldierName].asInt();
+
+			wave->soldiers.push_back(soldier);
+		}
+
+		for (Json::Value::const_iterator itr = cfg_root[waveName]["Conditions"].begin(); itr != cfg_root[waveName]["Conditions"].end(); itr++) {
+			std::string condition = itr.name();
+			int amount = cfg_root[waveName]["Conditions"][condition].asInt();
+
+			Condition* co = new Condition(condition, amount);
+			std::cout << "con: " << condition << " - " << co->condition << std::endl;
+			
+			wave->conditions.push_back(co);
+		}
+
+		waves.push_back(wave);
 	}
 }
 
@@ -113,11 +157,46 @@ LoaderManager& getLoaderManager() {
 	return manager;
 }
 
-
 Effect* LoaderManager::getEffects(std::string type) {
 	for (int i = 0; i < effects.size(); i++)
 		if (effects[i]->type == type)
 			return effects[i];
 
 	return nullptr;
+}
+
+std::vector<Wave*> LoaderManager::getWaves() {
+	std::vector<Wave*> returnWaves;
+
+	for (int i = 0; i < waves.size(); i++) {
+		bool conditionsMet = true;
+
+		for (int j = 0; j < waves[i]->conditions.size(); j++) {
+			std::string condition = waves[i]->conditions[j]->name;
+			int amount = waves[i]->conditions[j]->condition;
+
+			std::cout << "Condition " << condition << " - " << amount << std::endl;
+			
+			//Check for condition
+			if (condition == "Buildings") {
+				if (GameData::Buildings.size() > amount) conditionsMet = false;
+			}
+			else if (condition == "Mines") {
+				if (GameData::Mines.size() > amount) conditionsMet = false;
+			} //else if (condition == "Gold") {
+				//if (GameInstance::Gold != amount) conditionsMet = false;
+			//} 
+			else {
+				conditionsMet = false;
+
+				std::cout << "[DEBUG] Errro couldn't find a condition with the name of " << condition << " inside a wave with the name of " << waves[i]->name << std::endl;
+			}
+		}
+
+		//If all conditions we met, add it to the list.
+		if (conditionsMet)
+			returnWaves.push_back(waves[i]);
+	}
+
+	return returnWaves;
 }
